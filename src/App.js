9282fc94ca3142ee895f6e61c28e9c86a34db6bd
Shreeya9094 <shreeyadash.116@gmail.com>
2022-12-04 10:10:16 +0530

    eshop

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import Home from "./Home";
+import Header from "./Header";
+import Checkout from "./Checkout";
+
+import { BrowserRouter as Router,  Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        
+          <Route path = "/checkout">
+          <Header/>
+          <Checkout/>
+          </Route>
+          <Route path = "/">
+          <Header/>
+          <Home/>
+          </Route>
+     
+      </Router>
+      
+    </div>  
+
+
+  );
+}
+
+export default App;
